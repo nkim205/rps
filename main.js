677d8d2c89b8319315cbf61866dd031765666ad9
Nathan D. Kim <kimnathan0324@gmail.com>
2025-05-14 07:29:55 -1000

    fr fix image scale + improved game logic

--- main.js
+++ main.js
@@ -1,53 +1,50 @@
 function chooseRock() { 
-    var computerChoice;
     var computer = 10 * Math.random();
-    if (computer < 3) {
-        // computerChoice = "rock";
+    if ((computer >= 0 && computer < 1) || (computer >= 3 && computer < 4) || (computer >= 6 && computer < 7)) {
+        // computer choice = rock
         show("rock", "rock");
-    } else if (computer < 6) {
-        // computerChoice = "paper";
+    } else if ((computer >= 1 && computer < 2) || (computer >= 4 && computer < 5) || (computer >= 7 && computer < 8)) {
+        // computer choice = paper
         show("rock", "paper");
-    } else if (computer < 9) {
-        // computerChoice = "scissor";
+    } else if ((computer >= 2 && computer < 3) || (computer >= 5 && computer < 6) || (computer >= 8 && computer < 9)) {
+        // computer choice = scissor
         show("rock", "scissor");
     } else {
-        // computerChoice = "gun";
+        // computer choice = gun
         showGun();
     }
 }
 
 function choosePaper() { 
-    var computerChoice;
     var computer = 10 * Math.random();
-    if (computer < 3) {
-        // computerChoice = "rock";
+    if ((computer >= 0 && computer < 1) || (computer >= 3 && computer < 4) || (computer >= 6 && computer < 7)) {
+        // computer choice = rock
         show("paper", "rock");
-    } else if (computer < 6) {
-        // computerChoice = "paper";
+    } else if ((computer >= 1 && computer < 2) || (computer >= 4 && computer < 5) || (computer >= 7 && computer < 8)) {
+        // computer choice = paper
         show("paper", "paper");
-    } else if (computer < 9) {
-        // computerChoice = "scissor";
+    } else if ((computer >= 2 && computer < 3) || (computer >= 5 && computer < 6) || (computer >= 8 && computer < 9)) {
+        // computer choice = scissor
         show("paper", "scissor");
     } else {
-        // computerChoice = "gun";
+        // computer choice = gun
         showGun();
     }
 }
 
 function chooseScissor() { 
-    var computerChoice;
     var computer = 10 * Math.random();
-    if (computer < 3) {
-        // computerChoice = "rock";
+    if ((computer >= 0 && computer < 1) || (computer >= 3 && computer < 4) || (computer >= 6 && computer < 7)) {
+        // computer choice = rock
         show("scissor", "rock");
-    } else if (computer < 6) {
-        // computerChoice = "paper";
+    } else if ((computer >= 1 && computer < 2) || (computer >= 4 && computer < 5) || (computer >= 7 && computer < 8)) {
+        // computer choice = paper
         show("scissor", "paper");
-    } else if (computer < 9) {
-        // computerChoice = "scissor";
+    } else if ((computer >= 2 && computer < 3) || (computer >= 5 && computer < 6) || (computer >= 8 && computer < 9)) {
+        // computer choice = scissor
         show("scissor", "scissor");
     } else {
-        // computerChoice = "gun";
+        // computer choice = gun
         showGun();
     }
 }
@@ -83,22 +80,25 @@ function show(player, computer) {
     
     document.getElementById("resultText").textContent = "You " + result + "! The oppoenent chose " + computer + ".";
     document.getElementById("opponentImage").src = "./images/" + computer + ".svg";
-    document.getElementById("gameResult").style.visibility = "visible";
     document.getElementById("rockBtn").disabled = true;
     document.getElementById("paperBtn").disabled = true;
     document.getElementById("scissorBtn").disabled = true;
+    document.getElementById("gameResult").style.visibility = "visible";
+    document.getElementById("closeBtn").style.visibility = "visible";
 }
 
 function showGun() {
-    document.getElementById("resultText").textContent = "You lose! The oppoenent chose gun.";
+    document.getElementById("resultText").textContent = "You DIE! The oppoenent chose gun.";
     document.getElementById("opponentImage").src = "./images/gun.svg";
-    document.getElementById("gameResult").style.visibility = "visible";
     document.getElementById("rockBtn").disabled = true;
     document.getElementById("paperBtn").disabled = true;
     document.getElementById("scissorBtn").disabled = true;
+    document.getElementById("gameResult").style.visibility = "visible";
+    document.getElementById("closeBtn").style.visibility = "visible";
 }
 
 function closeWindow() {
+    document.getElementById("closeBtn").style.visibility = "hidden";
     document.getElementById("gameResult").style.visibility = "hidden";
     document.getElementById("opponentImage").src = "./images/eye.svg";
     document.getElementById("rockBtn").disabled = false;
